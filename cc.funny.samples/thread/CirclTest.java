7ef896cff8a6a6fd5319aec4d9eb196215ae8699
zhangpengzhi <510176668@qq.com>
2019-04-28 15:37:48 +0800

    add atomic

--- cc.funny.samples/thread/CirclTest.java
+++ cc.funny.samples/thread/CirclTest.java
@@ -1,11 +1,14 @@
 package thread;
 
 
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CirclTest implements Runnable{
 
+    private static final AtomicInteger retries = new AtomicInteger(10);
 
     public void run() {
+
 
     }
 }
